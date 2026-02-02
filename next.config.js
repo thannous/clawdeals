21cf308cf76d1a60c7a1d12df405c758b49f0373
tchau@france.groupe.intra <thanh.chau@vidal.fr>
2026-02-02 18:08:20 +0100

    chore: disable locale detection

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
     locales: ["fr", "en"],
     defaultLocale: "en",
     localeDetection: false
-  },
-  async redirects() {
-    return [
-      { source: "/en", destination: "/", permanent: true },
-      { source: "/en/:path*", destination: "/:path*", permanent: true },
-      { source: "/fr/robots.txt", destination: "/robots.txt", permanent: true },
-      { source: "/fr/sitemap.xml", destination: "/sitemap.xml", permanent: true }
-    ];
   }
 };
 
